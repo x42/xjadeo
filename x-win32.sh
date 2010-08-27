29fa0e033b2e2ce83217efee2d8912ebc05399ca
Robin Gareus <robin@gareus.org>
2010-08-27 15:00:01 +0200

    indenting & scripts cleanup

--- x-win32.sh
+++ x-win32.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #NORECONF=1
 
+WINEBASEDIR=/home/rgareus/.wine/drive_c/x-prefix
+NSISEXE=/home/rgareus/.wine/drive_c/Program\ Files/NSIS/makensis.exe
 unset CC
 if test -z "$NORECONF"; then
-PKG_CONFIG_PATH=/home/rgareus/.wine/drive_c/x-prefix/lib/pkgconfig/ CFLAGS="-I/home/rgareus/.wine/drive_c/x-prefix/include -I.." LDFLAGS="-L/home/rgareus/.wine/drive_c/x-prefix/lib/ -L/home/rgareus/.wine/drive_c/x-prefix/bin" \
+PKG_CONFIG_PATH=$WINEBASEDIR/lib/pkgconfig/ CFLAGS="-I$WINEBASEDIR/include -I.." LDFLAGS="-L$WINEBASEDIR/lib/ -L$WINEBASEDIR/bin" \
 	./configure --host=i586-mingw32msvc --build=i386-linux --prefix=/home/rgareus/.wine/drive_c/x-prefix/ \
-	--disable-xv --disable-imlib2 --disable-lash --disable-mq --disable-ipc --disable-osc \
-	--with-fontfile=./FreeMonoBold.ttf \
+	  --disable-xv --disable-imlib2 --disable-lash --disable-mq --disable-ipc --disable-osc \
+	  --with-fontfile=./FreeMonoBold.ttf \
 	|| exit
 
 # TODO FONTFILE 
@@ -18,7 +20,7 @@ NSIDIR=contrib/nsi/
 make -C src/xjadeo xjadeo.exe || exit
 cp -v src/xjadeo/xjadeo.exe $NSIDIR
 
-WINEBIN=/home/rgareus/.wine/drive_c/x-prefix/bin/
+WINEBIN=$WINEBASEDIR/bin/
 cp -v src/xjadeo/xjadeo.exe $NSIDIR
 cp -v $WINEBIN/avcodec-52.dll $NSIDIR
 cp -v $WINEBIN/avformat-52.dll $NSIDIR
@@ -42,8 +44,4 @@ cp -v $QTBIN/mingwm10.dll $NSIDIR
 VERSION=$(grep " VERSION " config.h | cut -d ' ' -f3 | sed 's/"//g'| sed 's/\./_/g')
 echo $VERSION
 cat $NSIDIR/xjadeo.nsi.tpl | sed 's/VERSION/'$VERSION'/' > $NSIDIR/xjadeo.nsi
-#qmake -spec win32-x-g++ -config release \
-#	&& make clean \
-#	&& make -j2 \
-#	&& wine /home/rgareus/.wine/drive_c/Program\ Files/NSIS/makensis.exe release/movemovie.nsi
-wine /home/rgareus/.wine/drive_c/Program\ Files/NSIS/makensis.exe $NSIDIR/xjadeo.nsi
+wine $NSISEXE $NSIDIR/xjadeo.nsi
