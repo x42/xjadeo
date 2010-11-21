a08511008de36a4f90e2c990159f1c69389c22a3
Robin Gareus <robin@gareus.org>
2010-11-21 12:52:46 +0100

    added russian translation.

---
+++ src/qt-gui/qjadeo_ru.ts
@@ -0,0 +1,455 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ImportDialog</name>
+    <message>
+        <location filename="importdialog.ui" line="14"/>
+        <source>Import - Convert Video File</source>
+        <translation>Импорт — Конвертирование видеофайла</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="23"/>
+        <source>Video File</source>
+        <translation>Видеофайл</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="29"/>
+        <source>Source:</source>
+        <translation>Источник:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="94"/>
+        <source>File Info:</source>
+        <translation>Информация:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="107"/>
+        <source>Destination:</source>
+        <translation>Конечный файл:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="165"/>
+        <source>Encoder Settings</source>
+        <translation>Параметры кодировщика</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="171"/>
+        <source>Width:</source>
+        <translation>Ширина:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="207"/>
+        <source>Scale</source>
+        <translation>Масштабировать</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="214"/>
+        <source>Height:</source>
+        <translation>Высота:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="253"/>
+        <source>Break Aspect Ratio</source>
+        <translation>Без учёта соотношения сторон</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="260"/>
+        <source>FPS:</source>
+        <translation>Кадров в секунду:</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="308"/>
+        <source>24</source>
+        <translation>24</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="313"/>
+        <source>25</source>
+        <translation>25</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="318"/>
+        <source>29.97</source>
+        <translation>29.97</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="323"/>
+        <source>30</source>
+        <translation>30</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="331"/>
+        <source>Resample</source>
+        <translation>Выполнять ресэмплинг</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="341"/>
+        <source>extra mencoder options</source>
+        <translation>Дополнительные ключи mencoder</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="371"/>
+        <source>&amp;Cancel</source>
+        <translation>О&amp;тменить</translation>
+    </message>
+    <message>
+        <location filename="importdialog.ui" line="400"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="importdialog.cpp" line="17"/>
+        <source>select video file to convert</source>
+        <translation>Выберите конвертируемый видеофайл</translation>
+    </message>
+    <message>
+        <location filename="importdialog.cpp" line="34"/>
+        <source>choose a destinaton filename</source>
+        <translation>Выберите имя конечного файла</translation>
+    </message>
+</context>
+<context>
+    <name>ImportProgress</name>
+    <message>
+        <location filename="importprogress.ui" line="14"/>
+        <source>Transcoding Video File</source>
+        <translation>Выполняется конвертирование файла</translation>
+    </message>
+    <message>
+        <location filename="importprogress.ui" line="38"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>qjadeo</source>
+        <translation>qjadeo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <source>screen updates per sec.</source>
+        <translation>Обновлений в секунду:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>time offset in video-frames:</source>
+        <translation>Смещение времени в видеокадрах:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="219"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>&amp;Sync</source>
+        <translation type="unfinished">С&amp;инхронизация</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="242"/>
+        <source>&amp;Zoom</source>
+        <translation>&amp;Масштаб</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="251"/>
+        <source>&amp;OSD</source>
+        <translation>&amp;OSD</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="261"/>
+        <source>&amp;Seek</source>
+        <translation>Пере&amp;ход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="269"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="287"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Открыть...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="293"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="301"/>
+        <source>&amp;Close xjadeo and Exit</source>
+        <translation>&amp;Закрыть xjadeo и выйти</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="304"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="315"/>
+        <source>&amp;About</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="318"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="332"/>
+        <location filename="mainwindow.ui" line="335"/>
+        <source>Frame number</source>
+        <translation>Номер кадра</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="346"/>
+        <location filename="mainwindow.ui" line="349"/>
+        <source>SMPTE</source>
+        <translation>SMPTE</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="360"/>
+        <location filename="mainwindow.ui" line="363"/>
+        <source>Filename</source>
+        <translation>Имя файла</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="374"/>
+        <location filename="mainwindow.ui" line="377"/>
+        <source>Box</source>
+        <translation>Рисовать фон</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="385"/>
+        <location filename="mainwindow.ui" line="388"/>
+        <source>Font...</source>
+        <translation>Шрифт...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="396"/>
+        <source>&amp;Import</source>
+        <translation>&amp;Импортировать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="399"/>
+        <source>Import</source>
+        <translation>Импортировать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="402"/>
+        <source>Convert video files.</source>
+        <translation>Сконвертировать видеофайлы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="410"/>
+        <source>&amp;Configure qjadeo</source>
+        <translation>&amp;Настроить qjadeo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="413"/>
+        <source>Configure qjadeo</source>
+        <translation>Настроить qjadeo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="421"/>
+        <source>Disconnect and &amp;Exit</source>
+        <translation>Отсоединиться и в&amp;ыйти</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="424"/>
+        <source>Disconnect</source>
+        <translation>Отсоединиться</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="435"/>
+        <location filename="mainwindow.ui" line="438"/>
+        <source>to any frame</source>
+        <translation>К любому кадру</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="449"/>
+        <location filename="mainwindow.ui" line="452"/>
+        <source>keyframes only</source>
+        <translation>Только по ключевым кадрам</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="463"/>
+        <location filename="mainwindow.ui" line="466"/>
+        <source>continuously</source>
+        <translation>Непрерывно</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="478"/>
+        <location filename="mainwindow.ui" line="481"/>
+        <source>JACK Transport</source>
+        <translation>Транспорт JACK</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="492"/>
+        <location filename="mainwindow.ui" line="495"/>
+        <source>LTC (JACK)</source>
+        <translation>LTC (JACK)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="506"/>
+        <location filename="mainwindow.ui" line="509"/>
+        <source>MTC (JACK MIDI)</source>
+        <translation>MTC (JACK MIDI)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="520"/>
+        <location filename="mainwindow.ui" line="523"/>
+        <source>MTC (ALSA SEQ)</source>
+        <translation>MTC (ALSA SEQ)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="534"/>
+        <source>off (slider)</source>
+        <translation>Выкл (ползунок)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="537"/>
+        <source>off</source>
+        <translation>Выкл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="550"/>
+        <location filename="mainwindow.ui" line="553"/>
+        <source>50%</source>
+        <translation>50%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="561"/>
+        <location filename="mainwindow.ui" line="564"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="572"/>
+        <location filename="mainwindow.ui" line="575"/>
+        <source>200%</source>
+        <translation>200%</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="583"/>
+        <location filename="mainwindow.ui" line="586"/>
+        <source>Maximize</source>
+        <translation>Во весь экран</translation>
+    </message>
+</context>
+<context>
+    <name>PrefDialog</name>
+    <message>
+        <location filename="prefdialog.ui" line="14"/>
+        <source>Preferences</source>
+        <translation>Параметры</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="23"/>
+        <source>General Settings</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="31"/>
+        <source>Autoconnect ALSA Midi port (MTC):</source>
+        <translation>Автосоединение с портом ALSA MIDI (MTC):</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="80"/>
+        <source>Autoconnect JACK Midi port (MTC):</source>
+        <translation>Автосоединение с портом JACK MIDI (MTC):</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="129"/>
+        <source>xjadeo/xjremote executable:</source>
+        <translation>Файл xjadeo/xjremote:</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="175"/>
+        <source>xjinfo executable:</source>
+        <translation>Файл xjinfo:</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="221"/>
+        <source>mencoder executable:</source>
+        <translation>Файл mencoder:</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="268"/>
+        <source>Import Settings</source>
+        <translation>Импорт</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="274"/>
+        <source>Import to Codec:</source>
+        <translation>Используемый кодек:</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="320"/>
+        <source>mpeg4</source>
+        <translation>mpeg4</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="325"/>
+        <source>mjpeg</source>
+        <translation>mjpeg</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="330"/>
+        <source>msmpeg4</source>
+        <translation>msmpeg4</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="335"/>
+        <source>mpeg1video</source>
+        <translation>mpeg1video</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="340"/>
+        <source>mpeg2video</source>
+        <translation>mpeg2video</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="348"/>
+        <source>mencoder options:</source>
+        <translation>Параметры mencoder:</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="396"/>
+        <source>Default Import Destination Path:</source>
+        <translation>Каталог хранения импортированных файлов</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="463"/>
+        <source>&amp;Cancel</source>
+        <translation>О&amp;тменить</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.ui" line="492"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="prefdialog.cpp" line="42"/>
+        <source>Select default destination folder</source>
+        <translation>Выберите папку для хранения импортированных файлов</translation>
+    </message>
+</context>
+<context>
+    <name>QJadeo</name>
+    <message>
+        <location filename="qjadeo.cpp" line="136"/>
+        <source>&amp;%1 %2</source>
+        <translation>&amp;%1 %2</translation>
+    </message>
+</context>
+</TS>
