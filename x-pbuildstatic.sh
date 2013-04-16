21f05d04fb6060de29cce1b9e322952e18ad6497
Robin Gareus <robin@gareus.org>
2013-04-16 18:10:47 +0200

    scripts for static builds

---
+++ x-pbuildstatic.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# this script creates a statically linked version of xjadeo
+#
+# It is intended to run in a pristine chroot or VM of a minimal
+# debian system. see http://wiki.debian.org/cowbuilder
+#
+# This script
+#  - git clone the source to $SRC (default /usr/src)
+#  - build and install ffmpeg to $PFX (default ~/local/)
+#  - build xjadeo and bundle it to /tmp/xjadeo*.tgz
+#    (/tmp/ is fixed set by x-static.sh )
+#
+
+#use environment variables if set for SRC and PFX
+: ${SRC=/usr/src}
+: ${PFX=$HOME/local}
+
+if [ "$(id -u)" != "0" -a -z "$SUDO" ]; then
+	echo "This script must be run as root in pbuilder" 1>&2
+  echo "e.g sudo cowbuilder --architecture amd64 --distribution wheezy --bindmounts /tmp --execute $0"
+	exit 1
+fi
+
+$SUDO apt-get -y install git build-essential yasm \
+	libass-dev libbluray-dev libgmp3-dev \
+	libbz2-dev libfreetype6-dev libgsm1-dev liblzo2-dev \
+	libmp3lame-dev libopenjpeg-dev libopus-dev librtmp-dev \
+	libschroedinger-dev libspeex-dev libtheora-dev \
+	libvorbis-dev libvpx-dev libx264-dev \
+	libxvidcore-dev zlib1g-dev zlib1g-dev \
+	libpng12-dev libjpeg8-dev \
+	libxv-dev libjack-jackd2-dev libx11-dev  libfreetype6-dev \
+	libltc-dev libxpm-dev liblo-dev autoconf automake
+
+cd $SRC
+git clone -b release/1.2 --depth 0 git://source.ffmpeg.org/ffmpeg
+git clone -b master --depth 0 git://xjadeo.git.sourceforge.net/gitroot/xjadeo/xjadeo
+
+cd $SRC/xjadeo
+VERSION=$(git describe --tags HEAD)
+git archive --format=tar --prefix=xjadeo-${VERSION}/ HEAD | gzip -9 > /tmp/xjadeo-${VERSION}.tar.gz
+
+cd $SRC/ffmpeg
+#FFVERSION=$(git describe --tags)
+#git archive --format=tar --prefix=ffmpeg-${FFVERSION}/ HEAD | gzip -9 > /tmp/ffmpeg-${FFVERSION}.tar.gz
+
+./configure --enable-gpl \
+	--enable-libmp3lame --enable-libx264 --enable-libxvid --enable-libtheora  --enable-libvorbis \
+	--enable-libvpx --enable-libopenjpeg --enable-libopus --enable-libschroedinger \
+	--enable-libspeex --enable-libbluray --enable-libgsm \
+	--disable-vaapi --disable-x11grab \
+	--disable-devices \
+	--enable-shared --enable-static --prefix=$PFX $@
+
+make -j4 || exit 1
+make install || exit 1
+
+cd $SRC/xjadeo
+autoreconf -i
+./x-static.sh || exit 1
+
+ls -l /tmp/xjadeo*.tgz
---
+++ x-static.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+#NORECONF=1
+
+#path to the static ffmpeg installation
+: ${PFX=$HOME/local}
+#path to output directory -- /xjadeo*.tgz will end up there
+: ${RESULT=/tmp}
+
+LIBF=$PFX/lib
+BINF=$PFX/bin
+export PKG_CONFIG_PATH=${LIBF}/pkgconfig
+
+if test -z "$NORECONF"; then
+	./configure \
+		--disable-imlib2 --disable-lash --disable-mq --disable-ipc --disable-sdl \
+		--disable-qtgui --disable-jacksession --disable-portmidi --disable-alsamidi \
+	  --with-fontfile=/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf \
+	|| exit
+fi
+
+make -C src/xjadeo xjadeo
+
+SRCVERSION=$(grep " VERSION " config.h | cut -d ' ' -f3 | sed 's/"//g')
+VERSION=$(git describe --tags HEAD)
+TRIPLET=$(gcc -print-multiarch)
+echo $VERSION
+OUTFN=xjadeo-$TRIPLET-$VERSION
+
+# ffmpeg needs this libs
+LIBDEPS=" \
+ libpng12.a \
+ libjpeg.a \
+ libmp3lame.a \
+ libspeex.a \
+ libtheoraenc.a \
+ libtheoradec.a \
+ libogg.a \
+ libvorbis.a \
+ libvorbisenc.a \
+ libvorbisfile.a \
+ libschroedinger-1.0.a \
+ liborc-0.4.a \
+ libgsm.a \
+ libbluray.a \
+ libxvidcore.a \
+ libopus.a \
+ libbz2.a \
+ libvpx.a \
+ libopenjpeg.a \
+ libx264.a \
+ libz.a \
+ libfreetype.a \
+ libltc.a \
+ liblo.a \
+ libXpm.a \
+ libX11.a \
+ libxcb.a \
+ libXau.a \
+ libXv.a \
+ libXext.a \
+ libXdmcp.a \
+ "
+
+# resolve paths to static libs on the system
+SLIBS=""
+for SLIB in $LIBDEPS; do
+	echo "searching $SLIB.."
+	SL=`find /usr/lib -name "$SLIB"`
+	if test -z "$SL"; then
+		echo "not found."
+		exit 1
+	fi
+	SLIBS="$SLIBS $SL"
+done
+
+
+cd src/xjadeo
+rm -f $OUTFN
+gcc -Wall -O3 \
+ -o $OUTFN \
+ -DHAVE_CONFIG_H \
+	"-DSUBVERSION=\"$SRCVERSION\"" \
+	`pkg-config --cflags freetype2 jack` \
+	 xjadeo-xjadeo.o xjadeo-display.o xjadeo-jack.o xjadeo-midi.o xjadeo-freetype.o xjadeo-display_x11.o \
+	 xjadeo-smpte.o xjadeo-main.o xjadeo-remote.o xjadeo-configfile.o xjadeo-lash.o xjadeo-mqueue.o \
+	 xjadeo-display_mac.o xjadeo-xjosc.o xjadeo-display_sdl.o xjadeo-ltc-jack.o \
+	${LIBF}/libavformat.a \
+	${LIBF}/libavcodec.a \
+	${LIBF}/libswscale.a \
+	${LIBF}/libavdevice.a \
+	${LIBF}/libavutil.a \
+	\
+	$SLIBS \
+	`pkg-config --libs jack` -lpthread -lm  \
+|| exit 1
+
+strip $OUTFN
+ls -lh $OUTFN
+ldd $OUTFN
+
+# give any arg to disable bundle
+test -n "$1" && exit 1
+
+# build .tgz bundle
+rm -rf $RESULT/$OUTFN $RESULT/$OUTFN.tgz
+mkdir $RESULT/$OUTFN
+cp $OUTFN $RESULT/$OUTFN/xjadeo
+cp ../../doc/xjadeo.1 $RESULT/$OUTFN/xjadeo.1
+cd $RESULT/ ; tar czf $RESULT/$OUTFN.tgz $OUTFN ; cd -
+rm -rf $RESULT/$OUTFN
+ls -lh $RESULT/$OUTFN.tgz
