0bce596644877db2d116c9f5b44dfe3f6aeaff27
Robin Gareus <robin@gareus.org>
2014-08-12 19:31:40 +0200

    NOOP, whitespace consistency

--- src/xjadeo/display_gl_osx.m
+++ src/xjadeo/display_gl_osx.m
@@ -20,7 +20,7 @@
 #include "display_gl_common.h"
 #if (defined HAVE_GL && defined PLATFORM_OSX)
 
-void xapi_open(void *d);
+void xapi_open (void *d);
 extern double framerate;
 
 #import <Cocoa/Cocoa.h>
@@ -35,14 +35,14 @@ static NSAutoreleasePool *pool = nil;
 
 static pthread_mutex_t osxgui_sync = PTHREAD_MUTEX_INITIALIZER;
 static volatile uint8_t sync_sem;
-static void gl_sync_lock()   { if (osxgui_status > 0) { sync_sem = 1; pthread_mutex_lock(&osxgui_sync); }}
-static void gl_sync_unlock() { if (osxgui_status > 0) { sync_sem = 0; pthread_mutex_unlock(&osxgui_sync); }}
+static void gl_sync_lock ()   { if (osxgui_status > 0) { sync_sem = 1; pthread_mutex_lock (&osxgui_sync); }}
+static void gl_sync_unlock () { if (osxgui_status > 0) { sync_sem = 0; pthread_mutex_unlock (&osxgui_sync); }}
 
 #define PTLL gl_sync_lock()
 #define PTUL gl_sync_unlock()
 
 //forward declaration, need for 64bit apps on 10.5
-OSErr UpdateSystemActivity(UInt8 d);
+OSErr UpdateSystemActivity (UInt8 d);
 
 @interface MenuDLG : NSObject
 {
@@ -71,10 +71,10 @@ __attribute__ ((visibility ("hidden")))
 
 @implementation XjadeoWindow
 
-- (id)initWithContentRect:(NSRect)contentRect
-                styleMask:(unsigned int)aStyle
-                  backing:(NSBackingStoreType)bufferingType
-                    defer:(BOOL)flag
+- (id) initWithContentRect:(NSRect)contentRect
+                 styleMask:(unsigned int)aStyle
+                   backing:(NSBackingStoreType)bufferingType
+                     defer:(BOOL)flag
 {
 	@try {
 		NSWindow* w = [super initWithContentRect:contentRect
@@ -92,7 +92,7 @@ __attribute__ ((visibility ("hidden")))
 	return nil;
 }
 
-- (BOOL)windowShouldClose:(id)sender
+- (BOOL) windowShouldClose:(id)sender
 {
 	if ((interaction_override&OVR_QUIT_WMG) == 0) {
 		loop_flag = 0;
@@ -101,7 +101,7 @@ __attribute__ ((visibility ("hidden")))
 	return NO;
 }
 
-- (void)becomeKeyWindow:(id)sender
+- (void) becomeKeyWindow:(id)sender
 {
 }
 
@@ -109,13 +109,13 @@ __attribute__ ((visibility ("hidden")))
 	return YES;
 }
 
-- (void)miniaturize:(id)sender
+- (void) miniaturize:(id)sender
 {
 	loop_run = 0;
 	[super miniaturize:sender];
 }
 
-- (void)deminiaturize:(id)sender
+- (void) deminiaturize:(id)sender
 {
 	loop_run = 1;
 	[super deminiaturize:sender];
@@ -196,7 +196,7 @@ __attribute__ ((visibility ("hidden")))
 	pthread_mutex_unlock(&osx_vbuf_lock);
 }
 
--(void)updateTrackingAreas
+-(void) updateTrackingAreas
 {
 	if (trackingArea != nil) {
 		[self removeTrackingArea:trackingArea];
@@ -204,28 +204,28 @@ __attribute__ ((visibility ("hidden")))
 	}
 
 	const int opts = (NSTrackingMouseEnteredAndExited | NSTrackingMouseMoved | NSTrackingActiveAlways);
-	trackingArea = [ [NSTrackingArea alloc] initWithRect:[self bounds]
+	trackingArea = [[NSTrackingArea alloc] initWithRect:[self bounds]
 	                                             options:opts
 	                                               owner:self
 	                                            userInfo:nil];
 	[self addTrackingArea:trackingArea];
 }
 
-- (void)mouseEntered:(NSEvent*)theEvent
+- (void) mouseEntered:(NSEvent*)theEvent
 {
 	[self updateTrackingAreas];
-	gl_mousepointer(hide_mouse);
+	gl_mousepointer (hide_mouse);
 }
 
-- (void)mouseExited:(NSEvent*)theEvent
+- (void) mouseExited:(NSEvent*)theEvent
 {
-	CGDisplayShowCursor(kCGDirectMainDisplay);
+	CGDisplayShowCursor (kCGDirectMainDisplay);
 }
 
 - (void) mouseDragged:(NSEvent*)event
 {
 	NSRect frame = [[self window] frame];
-	NSPoint mp   = [self convertPoint: [event locationInWindow] fromView: self];
+	NSPoint mp   = [self convertPoint:[event locationInWindow] fromView:self];
 	if (!resizing && (frame.size.width - mp.x > 16 || mp.y > 16)) return;
 	resizing = TRUE;
 
@@ -244,11 +244,11 @@ __attribute__ ((visibility ("hidden")))
 		frame.size.height = 60;
 		frame.origin.y = originY;
 	}
-	[[self window] setFrame: frame display: YES animate: NO];
+	[[self window] setFrame:frame display:YES animate:NO];
 }
 - (void) mouseDown:(NSEvent*)event
 {
-	xjglButton(1);
+	xjglButton (1);
 }
 
 - (void) mouseUp:(NSEvent*)event
@@ -429,32 +429,32 @@ static void update_sync_menu() {
 	}
 }
 
-static void update_osd_menu() {
-	[mOsdTC     setState: (OSD_mode&OSD_SMPTE) ? NSOnState : NSOffState];
-	[mOsdVtcOff setState: (OSD_mode&(OSD_SMPTE|OSD_VTC)) ? NSOffState : NSOnState];
-	[mOsdVtcTc  setState: (OSD_mode&OSD_FRAME) ? NSOnState : NSOffState];
-	[mOsdVtcFn  setState: (OSD_mode&OSD_VTC)   ? NSOnState : NSOffState];
-	[mOsdOffOff setState: (OSD_mode&(OSD_OFFF|OSD_OFFS)) ? NSOffState : NSOnState];
-	[mOsdOffTc  setState: (OSD_mode&OSD_OFFS)  ? NSOnState : NSOffState];
-	[mOsdOffFn  setState: (OSD_mode&OSD_OFFF)  ? NSOnState : NSOffState];
-	[mOsdBox    setState: (OSD_mode&OSD_BOX)   ? NSOnState : NSOffState];
+static void update_osd_menu () {
+	[mOsdTC     setState:(OSD_mode & OSD_SMPTE) ? NSOnState : NSOffState];
+	[mOsdVtcOff setState:(OSD_mode & (OSD_SMPTE|OSD_VTC)) ? NSOffState : NSOnState];
+	[mOsdVtcTc  setState:(OSD_mode & OSD_FRAME) ? NSOnState : NSOffState];
+	[mOsdVtcFn  setState:(OSD_mode & OSD_VTC)   ? NSOnState : NSOffState];
+	[mOsdOffOff setState:(OSD_mode & (OSD_OFFF|OSD_OFFS)) ? NSOffState : NSOnState];
+	[mOsdOffTc  setState:(OSD_mode & OSD_OFFS)  ? NSOnState : NSOffState];
+	[mOsdOffFn  setState:(OSD_mode & OSD_OFFF)  ? NSOnState : NSOffState];
+	[mOsdBox    setState:(OSD_mode & OSD_BOX)   ? NSOnState : NSOffState];
 }
 
-static void update_dpy_menu() {
-	[mDpyLetterbox   setState: Xgetletterbox()    ? NSOnState : NSOffState];
-	[mDpyOnTop       setState: Xgetontop()        ? NSOnState : NSOffState];
-	[mDpyFullscreen  setState: Xgetfullscreen()   ? NSOnState : NSOffState];
-	[mDpyMouseCursor setState: Xgetmousepointer() ? NSOnState : NSOffState];
+static void update_dpy_menu () {
+	[mDpyLetterbox   setState:Xgetletterbox()    ? NSOnState : NSOffState];
+	[mDpyOnTop       setState:Xgetontop()        ? NSOnState : NSOffState];
+	[mDpyFullscreen  setState:Xgetfullscreen()   ? NSOnState : NSOffState];
+	[mDpyMouseCursor setState:Xgetmousepointer() ? NSOnState : NSOffState];
 }
 
 @implementation MenuDLG
-- (void)setMenuId:(int)id
+- (void)setMenuId: (int)id
 {
 	menuID = id;
 }
-- (void)menuWillOpen:(NSMenu *)menu
+- (void) menuWillOpen: (NSMenu *)menu
 {
-	switch(menuID) {
+	switch (menuID) {
 		case 1:
 			if (interaction_override&OVR_LOADFILE) {
 				[mFileOpen setEnabled:NO];
@@ -484,14 +484,14 @@ static void update_dpy_menu() {
 @implementation NSApplication (XJ)
 
 /* Invoked from the Quit menu item */
-- (void)terminate:(id)sender
+- (void) terminate: (id)sender
 {
 	if ((interaction_override&OVR_QUIT_OSX) == 0) {
 		loop_flag = 0;
 	}
 }
 
-- (void)showAbout:(id)sender
+- (void) showAbout: (id)sender
 {
 	NSAttributedString *credits = [[NSAttributedString alloc]
 		initWithString:@"(C) 2006-2014\nRobin Gareus & Luis Garrido"
@@ -508,7 +508,7 @@ static void update_dpy_menu() {
 }
 
 
-- (void)openVideo:(id)sender
+- (void) openVideo: (id)sender
 {
 	if (interaction_override&OVR_LOADFILE) return;
 	NSOpenPanel *panel = [NSOpenPanel openPanel];
@@ -521,9 +521,9 @@ static void update_dpy_menu() {
 	if (clicked == NSFileHandlingPanelOKButton) {
 		for (NSURL *url in [panel URLs]) {
 			if (![url isFileURL]) continue;
-			NSLog(@"%@", url.path);
+			//NSLog(@"%@", url.path);
 			const char *fn= [url.path UTF8String];
-			PTLL; xapi_open((void*)fn); PTUL;
+			PTLL; xapi_open ((void*)fn); PTUL;
 			break;
 		}
 	}
@@ -532,69 +532,69 @@ static void update_dpy_menu() {
 	}
 }
 
-- (void)syncJack:(id)sender { PTLL; ui_sync_to_jack(); PTUL; }
-- (void)syncLTC:(id)sender  { PTLL; ui_sync_to_ltc(); PTUL; }
-- (void)syncMTCJ:(id)sender { PTLL; ui_sync_to_mtc_jack(); PTUL; }
-- (void)syncMTCP:(id)sender { PTLL; ui_sync_to_mtc_portmidi(); PTUL; }
-- (void)syncNone:(id)sender { PTLL; ui_sync_none(); PTUL; }
+- (void) syncJack: (id)sender { PTLL; ui_sync_to_jack(); PTUL; }
+- (void) syncLTC: (id)sender  { PTLL; ui_sync_to_ltc(); PTUL; }
+- (void) syncMTCJ: (id)sender { PTLL; ui_sync_to_mtc_jack(); PTUL; }
+- (void) syncMTCP: (id)sender { PTLL; ui_sync_to_mtc_portmidi(); PTUL; }
+- (void) syncNone: (id)sender { PTLL; ui_sync_none(); PTUL; }
 
-- (void)osdVtcNone:(id)sender { PTLL; ui_osd_vtc_off(); PTUL; }
-- (void)osdVtcTc:(id)sender   { PTLL; ui_osd_vtc_tc(); PTUL; }
-- (void)osdVtcFn:(id)sender   { PTLL; ui_osd_vtc_fn(); PTUL; }
-- (void)osdTc:(id)sender      { PTLL; ui_osd_tc(); PTUL; }
-- (void)osdPos:(id)sender     { PTLL; ui_osd_permute(); PTUL; }
-- (void)osdOffNone:(id)sender { PTLL; ui_osd_offset_none(); PTUL; }
-- (void)osdOffFn:(id)sender   { PTLL; ui_osd_offset_fn(); PTUL; }
-- (void)osdOffTc:(id)sender   { PTLL; ui_osd_offset_tc(); PTUL; }
-- (void)osdBox:(id)sender     { PTLL; ui_osd_box(); PTUL; }
-- (void)osdClear:(id)sender   { PTLL; ui_osd_clear(); PTUL; }
+- (void) osdVtcNone: (id)sender { PTLL; ui_osd_vtc_off(); PTUL; }
+- (void) osdVtcTc: (id)sender   { PTLL; ui_osd_vtc_tc(); PTUL; }
+- (void) osdVtcFn: (id)sender   { PTLL; ui_osd_vtc_fn(); PTUL; }
+- (void) osdTc: (id)sender      { PTLL; ui_osd_tc(); PTUL; }
+- (void) osdPos: (id)sender     { PTLL; ui_osd_permute(); PTUL; }
+- (void) osdOffNone: (id)sender { PTLL; ui_osd_offset_none(); PTUL; }
+- (void) osdOffFn: (id)sender   { PTLL; ui_osd_offset_fn(); PTUL; }
+- (void) osdOffTc: (id)sender   { PTLL; ui_osd_offset_tc(); PTUL; }
+- (void) osdBox: (id)sender     { PTLL; ui_osd_box(); PTUL; }
+- (void) osdClear: (id)sender   { PTLL; ui_osd_clear(); PTUL; }
 
-- (void)dpySize50:(id)sender      { XCresize_percent(50); }
-- (void)dpySize100:(id)sender     { XCresize_percent(100); }
-- (void)dpySize150:(id)sender     { XCresize_percent(150); }
-- (void)dpySizeDec:(id)sender     { XCresize_scale(-1); }
-- (void)dpySizeInc:(id)sender     { XCresize_scale( 1); }
-- (void)dpyAspect:(id)sender      { XCresize_aspect(0); }
-- (void)dpyLetterbox:(id)sender   { Xletterbox(2); }
-- (void)dpyOnTop:(id)sender       { Xontop(2); }
-- (void)dpyFullscreen:(id)sender  { Xfullscreen(2); }
-- (void)dpyMouseCursor:(id)sender { hide_mouse^=1; }
+- (void) dpySize50: (id)sender      { XCresize_percent(50); }
+- (void) dpySize100: (id)sender     { XCresize_percent(100); }
+- (void) dpySize150: (id)sender     { XCresize_percent(150); }
+- (void) dpySizeDec: (id)sender     { XCresize_scale(-1); }
+- (void) dpySizeInc: (id)sender     { XCresize_scale( 1); }
+- (void) dpyAspect: (id)sender      { XCresize_aspect(0); }
+- (void) dpyLetterbox: (id)sender   { Xletterbox(2); }
+- (void) dpyOnTop: (id)sender       { Xontop(2); }
+- (void) dpyFullscreen: (id)sender  { Xfullscreen(2); }
+- (void) dpyMouseCursor: (id)sender { hide_mouse^=1; }
 
-- (void)offZero:(id)sender { PTLL; XCtimeoffset( 0, 0); PTUL; }
-- (void)offPF:(id)sender   { PTLL; XCtimeoffset( 1, 0); PTUL; }
-- (void)offMF:(id)sender   { PTLL; XCtimeoffset(-1, 0); PTUL; }
-- (void)offPM:(id)sender   { PTLL; XCtimeoffset( 2, 0); PTUL; }
-- (void)offMM:(id)sender   { PTLL; XCtimeoffset(-2, 0); PTUL; }
-- (void)offPH:(id)sender   { PTLL; XCtimeoffset( 3, 0); PTUL; }
-- (void)offMH:(id)sender   { PTLL; XCtimeoffset(-3, 0); PTUL; }
+- (void) offZero: (id)sender { PTLL; XCtimeoffset( 0, 0); PTUL; }
+- (void) offPF: (id)sender   { PTLL; XCtimeoffset( 1, 0); PTUL; }
+- (void) offMF: (id)sender   { PTLL; XCtimeoffset(-1, 0); PTUL; }
+- (void) offPM: (id)sender   { PTLL; XCtimeoffset( 2, 0); PTUL; }
+- (void) offMM: (id)sender   { PTLL; XCtimeoffset(-2, 0); PTUL; }
+- (void) offPH: (id)sender   { PTLL; XCtimeoffset( 3, 0); PTUL; }
+- (void) offMH: (id)sender   { PTLL; XCtimeoffset(-3, 0); PTUL; }
 
-- (void)jackPlayPause:(id)sender  { jackt_toggle(); }
-- (void)jackPlay:(id)sender       { jackt_start(); }
-- (void)jackStop:(id)sender       { jackt_stop(); }
-- (void)jackRewind:(id)sender     { jackt_rewind(); }
+- (void) jackPlayPause: (id)sender  { jackt_toggle(); }
+- (void) jackPlay: (id)sender       { jackt_start(); }
+- (void) jackStop: (id)sender       { jackt_stop(); }
+- (void) jackRewind: (id)sender     { jackt_rewind(); }
 @end
 
-static void gl_make_current() {
+static void gl_make_current () {
 	NSOpenGLContext* context = [osx_glview openGLContext];
 	[context makeCurrentContext];
 }
 
-static void gl_swap_buffers() {
-	glSwapAPPLE();
+static void gl_swap_buffers () {
+	glSwapAPPLE ();
 }
 
 void gl_newsrc () {
-	pthread_mutex_lock(&osx_vbuf_lock);
-	free(osx_vbuf);
-	osx_vbuf_size = video_buffer_size();
+	pthread_mutex_lock (&osx_vbuf_lock);
+	free (osx_vbuf);
+	osx_vbuf_size = video_buffer_size ();
 	if (osx_vbuf_size > 0) {
-		osx_vbuf = malloc(osx_vbuf_size * sizeof(uint8_t));
-		gl_reallocate_texture(movie_width, movie_height);
+		osx_vbuf = (uint8_t*)malloc (osx_vbuf_size * sizeof(uint8_t));
+		gl_reallocate_texture (movie_width, movie_height);
 	}
-	pthread_mutex_unlock(&osx_vbuf_lock);
+	pthread_mutex_unlock (&osx_vbuf_lock);
 }
 
-static void makeAppMenu(void) {
+static void makeAppMenu (void) {
 	NSMenuItem *menuItem;
 	id menubar = [[NSMenu new] autorelease];
 	id appMenuItem = [[NSMenuItem new] autorelease];
@@ -821,7 +821,7 @@ static void makeAppMenu(void) {
 	[windowMenuItem release];
 }
 
-static void osx_close_window() {
+static void osx_close_window () {
 	[NSApp removeWindowsItem:osx_window];
 	[osx_window setIsVisible:NO];
 	[osx_window close];
@@ -858,24 +858,24 @@ static int osx_open_window () {
 
 	[window makeKeyAndOrderFront:window];
 
-	gl_init();
-	gl_resize(ffctv_width, ffctv_height);
-	if (gl_reallocate_texture(movie_width, movie_height)) {
-		osx_close_window();
+	gl_init ();
+	gl_resize (ffctv_width, ffctv_height);
+	if (gl_reallocate_texture (movie_width, movie_height)) {
+		osx_close_window ();
 		return -1;
 	}
-	gl_newsrc();
+	gl_newsrc ();
 
 	[window setIsVisible:YES];
 
-	if (start_fullscreen) { gl_set_fullscreen(1); }
-	if (start_ontop) { gl_set_ontop(1); }
+	if (start_fullscreen) { gl_set_fullscreen (1); }
+	if (start_ontop) { gl_set_ontop (1); }
 	return 0;
 }
 
-static void osx_post_event(int data1) {
+static void osx_post_event (int data1) {
 	NSEvent* event = [NSEvent otherEventWithType:NSApplicationDefined
-	                                    location:NSMakePoint(0,0)
+	                                    location:NSMakePoint (0,0)
 	                               modifierFlags:0
 	                                   timestamp:0.0
 	                                windowNumber:0
@@ -889,52 +889,53 @@ static void osx_post_event(int data1) {
 
 int gl_open_window () {
 	while (osxgui_status == -1000) {
-		usleep(10000);
+		usleep (10000);
 	}
 	if (!pool) pool = [[NSAutoreleasePool alloc] init];
 	if (osxgui_status > 0) return 0; // already open
-	osx_post_event(1);
+	osx_post_event (1);
 	while (osxgui_status == 0) {
-		usleep(10000);
+		usleep (10000);
 	}
 	return osxgui_status > 0 ? 0 : -1;
 }
 
-void osx_shutdown() {
+void osx_shutdown () {
 	if (pool && osxgui_status != -1000) {
-		osx_post_event(0);
+		osx_post_event (0);
 	}
 	if (pool) {
 		[pool release]; pool = NULL;
 	}
 }
 
-void gl_close_window() {
+void gl_close_window () {
 	if (osxgui_status <= 0) {
 		return;
 	}
-	osx_post_event(2);
+	osx_post_event (2);
 	while (osxgui_status) {
-		usleep(10000);
+		usleep (10000);
 	}
-	free(osx_vbuf); osx_vbuf = NULL;
+	free (osx_vbuf);
+	osx_vbuf = NULL;
 	osx_vbuf_size = 0;
 }
 
 void gl_handle_events () {
-	pthread_mutex_unlock(&osxgui_sync); // XXX TODO first-time lock
+	pthread_mutex_unlock (&osxgui_sync); // XXX TODO first-time lock
 	while (sync_sem) {
-		sched_yield();
+		sched_yield ();
 	}
-	pthread_mutex_lock(&osxgui_sync);
-	osx_post_event(0); // TODO only on remote msg or similar event
+	pthread_mutex_lock (&osxgui_sync);
+	osx_post_event (0); // TODO only on remote msg or similar event
 }
 
 void osx_main () {
 	[NSAutoreleasePool new];
 	[NSApplication sharedApplication];
 	[NSApp setDelegate:[NSApplication sharedApplication]];
-	makeAppMenu();
+	makeAppMenu ();
 	[NSApp finishLaunching];
 
 	osxgui_status = 0;
@@ -948,7 +949,7 @@ void osx_main () {
 				switch(event.data1) {
 					case 1:
 						if (osxgui_status == 0) {
-							if (osx_open_window()) {
+							if (osx_open_window ()) {
 								osxgui_status = -1;
 							} else {
 								osxgui_status = 1;
@@ -957,15 +958,15 @@ void osx_main () {
 						break;
 					case 2:
 						if (osxgui_status > 0) {
-							osx_close_window();
+							osx_close_window ();
 							osxgui_status = 0;
 						}
 						break;
 					case 3:
-						pthread_mutex_lock(&osx_vbuf_lock);
-						xjglExpose(osx_vbuf);
-						pthread_mutex_unlock(&osx_vbuf_lock);
-						[osx_glview setNeedsDisplay: YES];
+						pthread_mutex_lock (&osx_vbuf_lock);
+						xjglExpose (osx_vbuf);
+						pthread_mutex_unlock (&osx_vbuf_lock);
+						[osx_glview setNeedsDisplay:YES];
 						break;
 					default:
 						break;
@@ -973,22 +974,22 @@ void osx_main () {
 				break;
 			}
 
-			[NSApp sendEvent: event];
+			[NSApp sendEvent:event];
 		}
-		while(event != nil && loop_flag);
+		while (event != nil && loop_flag);
 
 		// TODO: use a timer, and callbacks,
 		// currently triggered by osx_post_event() from gl_handle_events()
 		static int periodic_sync = 5;
 		if (--periodic_sync == 0) {
 			periodic_sync = framerate * 50;
-			UpdateSystemActivity(1 /*UsrActivity*/);
+			UpdateSystemActivity (1 /*UsrActivity*/);
 			// TODO use a one time call
 			// IOPMAssertionCreateWithName() NoDisplaySleepAssertion etc.
 		}
 	}
 	if (osxgui_status > 0) {
-		osx_close_window();
+		osx_close_window ();
 		osxgui_status = 0;
 	}
 	[NSAutoreleasePool release];
@@ -998,16 +999,15 @@ void osx_main () {
 
 void gl_render (uint8_t *buffer) {
 	if (!osx_vbuf) return;
-	pthread_mutex_lock(&osx_vbuf_lock);
-	memcpy(osx_vbuf, buffer, osx_vbuf_size * sizeof(uint8_t));
-	pthread_mutex_unlock(&osx_vbuf_lock);
-	[osx_glview setNeedsDisplay: YES];
-	//osx_post_event(3); // explicit expose, blocks
+	pthread_mutex_lock (&osx_vbuf_lock);
+	memcpy (osx_vbuf, buffer, osx_vbuf_size * sizeof(uint8_t));
+	pthread_mutex_unlock (&osx_vbuf_lock);
+	[osx_glview setNeedsDisplay:YES];
 }
 
 
 void gl_resize (unsigned int x, unsigned int y) {
-	[osx_window setContentSize:NSMakeSize(x, y) ];
+	[osx_window setContentSize:NSMakeSize (x, y) ];
 	[osx_glview reshape];
 }
 
@@ -1039,14 +1039,11 @@ void gl_set_ontop (int action) {
 	}
 }
 
-static NSRect nofs_frame;
-
 void gl_set_fullscreen (int action) {
 	// TODO check if we need performSelectorOnMainThread
 	if (action==2) _gl_fullscreen^=1;
 	else _gl_fullscreen = action ? 1 : 0;
 	if (_gl_fullscreen) {
-		nofs_frame = [osx_window frame];
 		[osx_glview setFullScreen:YES];
 	} else {
 		[osx_glview setFullScreen:NO];
@@ -1062,9 +1059,9 @@ void gl_mousepointer (int action) {
 	else hide_mouse = action ? 1 : 0;
 
 	if (hide_mouse) {
-		CGDisplayHideCursor(kCGDirectMainDisplay);
+		CGDisplayHideCursor (kCGDirectMainDisplay);
 	} else {
-		CGDisplayShowCursor(kCGDirectMainDisplay);
+		CGDisplayShowCursor (kCGDirectMainDisplay);
 	}
 }
 
