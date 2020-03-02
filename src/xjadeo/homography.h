ea37fe00cacc4454b5aed2521a1883344d4447f7
Ion Reguera <ion@stagelab.net>
2020-03-02 17:22:31 +0100

    updated homograpy.h license boilerplate

--- src/xjadeo/homography.h
+++ src/xjadeo/homography.h
@@ -1,10 +1,28 @@
 /*
- * homography.h
- *
- *  based on code from arturo castro
+ *   homography.h
+ *   Created on: 08/01/2010
+ *   Author: arturo castro
+ *   
+ * 	 https://github.com/arturoc/homography.h
+ * 	 http://forum.openframeworks.cc/index.php/topic,3121.0.html
  * 
+ * 
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
  *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>. 
  */
+
+
 #ifndef XJ_HOMOGRAPHY_H
 #define XJ_HOMOGRAPHY_H
 
